777371634382172911a8ae153a534b758ade71b6
hiai32 <hasumi2525888@gmail.com>
2024-06-25 14:14:54 +0900

    マイページの追加

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<int:user_id>/', views.home, name='home'),
     path('add/<int:user_id>/', views.add, name='add'),
     path('update/<int:user_id>/<int:task_id>/', views.update, name='update'),
+    path('mypage/<int:user_id>/', views.mypage, name='mypage'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,20 +1,28 @@
+import math
 from django.shortcuts import render, redirect
 from todolist.models import Task
+from account.models import User
 
 # Create your views here.
 def home(request, user_id):
     if request.method == 'POST':
-        if 'update' in request.POST:
-            task_id = request.POST['taskId']
+        task_id = request.POST['taskId']
+        if 'achieve' in request.POST:
+            task = Task.objects.get(pk=task_id)
+            task.isAchieved = 1
+            task.save()
+            user = User.objects.get(pk=user_id)
+            user.achievedTasks += 1
+            user.save()
+        elif 'update' in request.POST:
             return redirect(update, user_id, task_id)
         elif 'delete' in request.POST:
-            task_id = request.POST['taskId']
             task = Task.objects.get(pk=task_id)
             task.delete()
 
     context = {
-        'user_id': user_id,
-        'tasks': Task.objects.filter(userId=user_id)
+        'userId': user_id,
+        'tasks': Task.objects.filter(userId=user_id, isAchieved=0),
     }
 
     return render(request, 'todolist/home.html', context)
@@ -28,10 +36,14 @@ def add(request, user_id):
         task = Task(title=title, due=due, priority=priority, explanation=explanation, userId=user_id)
         task.save()
 
+        user = User.objects.get(pk=user_id)
+        user.addedTasks += 1
+        user.save()
+
         return redirect(home, user_id)
     
     context = {
-        'user_id': user_id
+        'userId': user_id,
     }
 
     return render(request, 'todolist/add.html', context)
@@ -48,8 +60,32 @@ def update(request, user_id, task_id):
         return redirect(home, user_id)
 
     context = {
-        'user_id': user_id,
-        'task': task
+        'userId': user_id,
+        'task': task,
     }
 
     return render(request, 'todolist/update.html', context)
+
+def mypage(request, user_id):
+    user = User.objects.get(pk=user_id)
+    
+    factorial = 1
+    level = 1
+    while factorial <= user.achievedTasks:
+        level += 1
+        factorial = math.factorial(level)
+
+
+    if user.addedTasks == 0:
+        achievementRate = 0
+    else:
+        achievementRate = user.achievedTasks / user.addedTasks * 100
+
+    context = {
+        'user': user,
+        'level': level,
+        'achievementRate': round(achievementRate, 1),
+        'tasks': Task.objects.filter(userId=user_id, isAchieved=1).order_by('-id'),
+    }
+    
+    return render(request, 'todolist/mypage.html', context)
